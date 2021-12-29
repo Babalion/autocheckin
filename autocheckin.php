7dbb29a96bfde4843d38f2e43df314ec5c1a9bb6
Babalion <48565736+Babalion@users.noreply.github.com>
2021-12-29 23:10:04 +0100

    Tested on Joomla 3.10 seems to work fine

--- autocheckin.php
+++ autocheckin.php
@@ -8,12 +8,8 @@
 // no direct access
 defined('_JEXEC') or die;
 
-use Joomla\CMS\Application\CMSApplication;
 use Joomla\CMS\Factory;
-use Joomla\CMS\Language\Text;
 use Joomla\CMS\Plugin\CMSPlugin;
-use Joomla\CMS\Http\HttpFactory;
-use Joomla\CMS\Version;
 
 class PlgSystemAutocheckin extends CMSPlugin
 {
@@ -21,89 +17,74 @@ class PlgSystemAutocheckin extends CMSPlugin
 	 * Load the language file on instantiation
 	 *
 	 * @var    boolean
-	 * @since  3.1
+	 * @since  Joomla! 3.1
 	 */
 	protected $autoloadLanguage = true;
 
 
+	private $app;
+	private $max_co_time_s;
+	private $active_element_types;
+
+	public function __construct(&$subject, $config = array())
+	{
+		parent::__construct($subject, $config);
+		$this->app           = Factory::getApplication();
+		$this->max_co_time_s = $this->params->get('max_co_time') * 20;
+
+		// read in element types
+		$checkin_articles   = $this->params->get('checkin_articles');
+		$checkin_categories = $this->params->get('checkin_categories');
+		$checkin_menus      = $this->params->get('checkin_menus');
+		$checkin_modules    = $this->params->get('checkin_modules');
+		$checkin_plugins    = $this->params->get('checkin_plugins');
+
+		// load the element-types
+		// third entry is the database-table-name of this elements
+		$this->active_element_types = array(
+			array('articles', $checkin_articles, 'id', '#__content'),
+			array('categories', $checkin_categories, 'id', '#__categories'),
+			array('menus', $checkin_menus, 'id', '#__menu'),
+			array('modules', $checkin_modules, 'id', '#__modules'),
+			array('plugins', $checkin_plugins, 'extension_id', '#__extensions')
+		);
+
+	}
+
 	public function onAfterRender(): bool
 	{
-		$this->app = Factory::getApplication();
+		// TODO is this needed?
 		if ($this->app->getName() == 'site')
 		{
 			return true;
 		}
-		$zeit   = Factory::getDate('now');
-		$maxco  = $this->params->get('maxco', 24);
-		$maxsec = $maxco * 3600;
 
-		$db = Factory::getDBO();
+		$time_now = Factory::getDate('now');
+		$db       = Factory::getDBO();
 
-		/**
-
-		// Artikel prüfen und einchecken
-		$query = 'SELECT id,checked_out,checked_out_time FROM #__content WHERE checked_out > 0';
-		$db->setQuery($query);
-		$erg = $db->loadObjectList();
-		$anz = count($erg);
-		for ($x = 0; $x < $anz; ++$x)
+		foreach ($this->active_element_types as $a_el_type)
 		{
-			$unter = strtotime($zeit) - strtotime($erg[$x]->checked_out_time);
-			if ($unter >= $maxsec)
+			// first index of $a_el_type[1]==1 if this element-type is toggled active in settings
+			if ($a_el_type[1] != 0)
 			{
-				$query = 'UPDATE #__content SET checked_out = 0 WHERE id = ' . $erg[$x]->id;
+				// select all checked out elements
+				$query = 'SELECT ' . $a_el_type[2] . ',checked_out,checked_out_time FROM ' . $a_el_type[3] . ' WHERE checked_out > 0';
 				$db->setQuery($query);
-				$erg1 = $db->query();
+				$checked_out_elements = $db->loadObjectList();
+
+				// now step over all elements and check in if max_co_time_s is reached
+				foreach ($checked_out_elements as $a)
+				{
+					$time_checked_out = strtotime($time_now) - strtotime($a->checked_out_time);
+					if ($time_checked_out >= $this->max_co_time_s)
+					{
+						$query = 'UPDATE  ' . $a_el_type[3] . ' SET checked_out = 0 WHERE ' . $a_el_type[2] . ' = ' . $a->{$a_el_type[2]};
+						$db->setQuery($query);
+						$db->execute();
+					}
+				}
 			}
 		}
-
-
-		// Module prüfen und einchecken
-		$query = 'SELECT id,checked_out,checked_out_time FROM #__modules WHERE checked_out > 0';
-		$db->setQuery($query);
-		$erg = $db->loadObjectList();
-		$anz = count($erg);
-		for ($x = 0; $x < $anz; ++$x)
-		{
-			$unter = strtotime($zeit) - strtotime($erg[$x]->checked_out_time);
-			if ($unter >= $maxsec)
-			{
-				$query = 'UPDATE #__modules SET checked_out = 0 WHERE id = ' . $erg[$x]->id;
-				$db->setQuery($query);
-				$erg1 = $db->getQuery($query);
-			}
-		}
-		// Plugins prüfen und einchecken, fehler korrigiert
-		$query = 'SELECT extension_id,checked_out,checked_out_time,type FROM #__extensions WHERE type = \'plugin\' AND checked_out > 0';
-		$db->setQuery($query);
-		$erg = $db->loadObjectList();
-		$anz = count($erg);
-		for ($x = 0; $x < $anz; ++$x)
-		{
-			$unter = strtotime($zeit) - strtotime($erg[$x]->checked_out_time);
-			if ($unter >= $maxsec)
-			{
-				$query = 'UPDATE #__extensions SET checked_out = 0 WHERE extension_id = ' . $erg[$x]->extension_id;
-				$db->setQuery($query);
-				$erg1 = $db->getQuery($query);
-			}
-		}
-		// Menüs prüfen und einchecken
-		$query = 'SELECT id,checked_out,checked_out_time,type FROM #__menu WHERE checked_out > 0';
-		$db->setQuery($query);
-		$erg = $db->loadObjectList();
-		$anz = count($erg);
-		for ($x = 0; $x < $anz; ++$x)
-		{
-			$unter = strtotime($zeit) - strtotime($erg[$x]->checked_out_time);
-			if ($unter >= $maxsec)
-			{
-				$query = 'UPDATE #__menu SET checked_out = 0 WHERE id = ' . $erg[$x]->id;
-				$db->setQuery($query);
-				$erg1 = $db->query();
-			}
-		}
-		 */
 
 		return true;
 	}
